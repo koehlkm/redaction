9cbbce15a5e66bef91cb2e889e7dccdf3f41c81c
koehlkm <60309591+koehlkm@users.noreply.github.com>
2021-05-26 17:16:50 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import spacy_streamlit
+from spacy_streamlit import visualize_ner
+import spacy
+
+st.title('Data Redaction App')
+st.subheader('Input Text and click enter to begin')
+nlp = spacy.load("en_core_web_trf")
+text = st.text_area('Input Text Here', height=400, max_chars=None, key=None, help=None)
+doc = nlp(text)
+visualize_ner(doc, labels=nlp.get_pipe("ner").labels)
+for ent in reversed(doc.ents):
+    # text = text[:ent.start_char] + ent.label_ + text[ent.end_char:]
+    text = text[:ent.start_char] + "#REDACTED#" + text[ent.end_char:]
+
+
+st.subheader('Redacted Results')
+st.write(text)
